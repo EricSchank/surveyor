f26323b290caf95629af2f6ae12d3aad03127f19
Eric Schank <erics@spiceworks.com>
2016-02-03 13:46:37 -0600

    fixing home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
-  before_filter :load_site
-  before_filter :load_text
+  # before_filter :load_site
+  # before_filter :load_text
 
   def index
   end
